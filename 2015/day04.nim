ea561a03448e4daa5d2ddcdc468797687649dc4b
Mark Janssen <mpc.janssen@gmail.com>
2024-08-06 22:05:16 +0200

    Day 2015-4 in nim

---
+++ 2015/day04.nim
@@ -0,0 +1,16 @@
+import md5
+import std/strutils
+
+const secret = "iwrupvqb"
+var start = 0
+var found5 = false
+var found6 = false
+while (not found5) or (not found6):
+  if (not found6) and getMD5(secret & $start).startsWith("000000"):
+    echo $start
+    found6 = true
+  if (not found5) and getMD5(secret & $start).startsWith("00000"):
+    echo $start
+    found5 = true
+  
+  start+=1
