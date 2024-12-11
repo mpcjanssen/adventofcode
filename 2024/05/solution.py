d8ddd06848c57f7c87a2c8e8d8b436f8ea28debf
Mark Janssen <mpc.janssen@gmail.com>
2024-12-11 21:41:38 +0100

    2024-05

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -1,15 +1,39 @@
+from functools import cmp_to_key
+
 rules,pages = open(0).read().split("\n\n")
 rules =  [ list(map(int, l.strip().split('|'))) for l in rules.split('\n')]
 pages =  [ list(map(int, l.strip().split(','))) for l in pages.strip().split('\n')]
 
-def valid(p,rules):
+
+
+def valid(p):
     for bef,aft in zip(p,p[1:]):
         if [bef,aft] not in rules: return False
     
     return True
 
+invalid = []
 part1 = 0
 for p in pages:
-    if valid(p,rules): part1 += p[(len(p)-1)//2]
+    if valid(p): 
+        part1 += p[(len(p)-1)//2]
+    else:
+        invalid += [p]
+
+def rep_sort(a,b):
+    if [a,b] in rules:
+        return -1
+    else:
+        return 1
 
 print(part1)
+
+cmp = cmp_to_key(rep_sort)
+
+part2 = 0
+for p in invalid:
+    np = sorted(p, key=cmp)
+    if valid(np): 
+        part2 += np[(len(np)-1)//2]
+
+print(part2)
