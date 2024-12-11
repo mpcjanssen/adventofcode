8c7921a57b5bf0b834f629cdc7c83e17d40e938a
Mark Janssen <mpc.janssen@gmail.com>
2024-12-11 20:21:16 +0100

    Simpler re

--- 2024/3/solution.py
+++ 2024/3/solution.py
@@ -5,22 +5,19 @@ input = open(0).read().strip()
 part1 = 0
 
 for m in re.findall(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)', input):
-    print(m)
     x,y = m
     part1 += int(x)*int(y)
 
 print(part1)
 
-for m in re.findall(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)', input):
-    print(m)
+for m in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', input):
     x,y = m
     part1 += int(x)*int(y)
 
 
 part2 = 0
 do = True
-print(part2)
-for m in re.findall(r'(do\(\)|don\'t\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\))', input):
+for m in re.findall(r'(do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\))', input):
     mstr,x,y = m
     if mstr == 'do()':
         do = True
