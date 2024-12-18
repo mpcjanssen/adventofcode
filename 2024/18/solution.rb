5d9deec142d971c7f010ba0325cf9c9646c70e8a
Mark Janssen <mpc.janssen@gmail.com>
2024-12-18 11:27:40 +0100

    2024-18 updates

--- 2024/18/solution.rb
+++ 2024/18/solution.rb
@@ -8,54 +8,48 @@ bytes = $stdin.readlines.map {|l|
 def solve(bytes,num,target)
   start = [0,0]
   xmax,ymax = target
-  # p [xmax,ymax]
+
   corrupted = bytes[0...num]
   grid = Array.new(ymax+1) {Array.new(xmax+1,'.')}
-  corrupted.each {|x,y|
-    grid[y][x] = '#'
-  }
+  corrupted.each { |x,y| grid[y][x] = '#' }
+
   dirs = [[1,0],[-1,0],[0,1],[0,-1]]
   scores = {start => 0}
-  heads = [start].to_set
+  heads = Queue.new
+  heads.push start
   while scores[target].nil? && heads.size > 0
-    # p heads
-    newheads = Set.new
-    heads.each { |head|
-      x,y = head
-      score = scores[[x,y]]
-      dirs.each { |dir|
-        dx,dy = dir
-        nx,ny = [x+dx,y+dy]
-        # p [nx,ny]
-        next if nx < 0 || nx > xmax
-        next if ny < 0 || ny > ymax
-        next if grid[ny][nx] == '#'
-        next if scores[[nx,ny]] != nil
-        newheads << [nx,ny]
-        scores[[nx,ny]] = score+1
-      }
-      # p newheads.size
-      heads = newheads
+    head = heads.pop
+    x,y = head
+    score = scores[[x,y]]
+    dirs.each { |dir|
+      dx,dy = dir
+      nx,ny = [x+dx,y+dy]
+      next if nx < 0 || nx > xmax
+      next if ny < 0 || ny > ymax
+      next if grid[ny][nx] == '#'
+      next if scores[[nx,ny]] != nil
+      heads.push [nx,ny]
+      scores[[nx,ny]] = score+1
     }
   end
   scores[target]
 
 end
 
-bpart1 = 1024
-# part1 = solve(bytes, bpart1, [6, 6] )
-part1 = solve(bytes, bpart1, [70, 70] )
+# target, bytes1 = [[6,6], 12]
+target, bytes1 = [[70,70], 1024]
+
+part1 = solve(bytes, bytes1, target )
 
 bfail = bytes.size-1
-bpass = bpart1
+bpass = bytes1
 
 # Do a binary search for last failing and last passing
 
 loop do
   bnext = bpass + (bfail - bpass)/2
-  # result = solve(bytes, bnext, [6, 6] )
-  result = solve(bytes, bnext, [70, 70] )
-  # p [bnext, result]
+  result = solve(bytes, bnext, target )
+
   if result != nil then
     bpass = bnext
   else
@@ -64,7 +58,8 @@ loop do
   break if bpass == bfail-1
 end
 
-
+# Take the bpass as index because the index of
+# the last byte after dropping n bytes is n-1
 part2 = bytes[bpass].join(",")
 
 p [part1,part2]
