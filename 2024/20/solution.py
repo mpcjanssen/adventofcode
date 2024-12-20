e58499efc43d7e060e1fddc23375d8c5e41e1b03
Mark Janssen <mpc.janssen@gmail.com>
2024-12-20 15:56:16 +0100

    2024-20 python

--- 2024/20/solution.py
+++ 2024/20/solution.py
@@ -1,18 +1,94 @@
-input = open(0).read()
-print(input)
+from collections import deque
+from collections import defaultdict
 
 
-## two observations
-# 1. A cheat needs to be adjacent to the car position 
-# next to the current fastest path otherwise the cheat has no effect
-#
-# A cheat needs to occur within the current fastest time otherwise it
-# will not give a reduction
+grid = [l.strip() for l in open(0).readlines()]
+# print(grid)
 
-## Observation 1 is incorrect: in the example below the cheat block is not on the current shortest path:
+start = None
+end = None
+racetrack = []
 
-######
-#    #
-# ## #
-# ## #
-#S CE#
+rows = len(grid)
+cols = len(grid[0])
+
+def manh(p1,p2):
+    r1,c1 = p1
+    r2,c2 = p2
+    return abs(r1-r2) + abs(c1-c2)
+
+
+for r in range(rows):
+    for c in range(cols):
+        p = (r,c)
+        v = grid[r][c]
+        if v == '#': continue
+        racetrack.append(p)
+        if v == 'E': 
+            end = p
+        if v == 'S':
+            start = p
+
+def scores(start,end):
+    dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+    scores = defaultdict(lambda: None)
+    scores[start] = 0
+    heads =  deque()
+    heads.append(start)
+    # print(heads)
+    while scores[end] == None and heads:
+        r,c = heads.pop()
+        # print(r,c)
+        score = scores[(r,c)]
+        for dr,dc in dirs:
+            nr, nc = r+dr,c+dc
+            if nr < 0 or nr >= rows: continue
+            if nc < 0 or nc >= cols: continue
+            if grid[nr][nc] == '#': continue
+            if scores[(nr,nc)] != None: continue
+            scores[(nr,nc)] = score+1
+            heads.append((nr,nc))
+       
+        # print(heads)
+
+    return scores
+
+def cheats(cheattime):
+    imps = defaultdict(lambda: 0)
+    tracksize = len(racetrack)
+    for idx in range(tracksize-1):
+        p1 = racetrack[idx]
+        options = racetrack[idx+1:]
+        startscore = sc[p1]
+        for p2 in options:
+            dist = manh(p1,p2)
+            if dist > cheattime: continue
+            endscore = cleanscore-sc[p2]
+            newscore = endscore+startscore + dist
+            delta = cleanscore-newscore
+            if delta > 0: imps[delta]+= 1
+    
+    return imps
+
+
+
+# print(start, end)
+sc = scores(start,end)
+cleanscore = sc[end]
+# print(cleanscore)
+
+racetrack.sort(key=lambda t: sc[t])
+# print(racetrack)
+part1 = 0
+for k,v in cheats(2).items():
+    if k >= 100: part1+=v
+
+print(part1)
+
+part2 = 0
+for k,v in cheats(20).items():
+    if k >= 100: part2+=v
+
+
+print(part2)
+
