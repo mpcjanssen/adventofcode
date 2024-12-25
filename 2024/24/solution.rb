730fd707778450ea25a6a043cab86cf92c15a089
Mark Janssen <mpc.janssen@gmail.com>
2024-12-25 20:04:21 +0100

    Rubocop

--- 2024/24/solution.rb
+++ 2024/24/solution.rb
@@ -1,36 +1,33 @@
-init,program = $stdin.read.split("\n\n")
+# frozen_string_literal: true
+
+init, program = $stdin.read.split("\n\n")
 
 regs = {}
-init.lines.each { |l|
-  name,val = l.strip.split(": ")
+init.lines.each do |l|
+  name, val = l.strip.split(': ')
   regs[name] = val.to_i
-}
-
-program.lines.each { | l |
-  inst,reg = l.strip.split(" -> ")
-  regs[reg] = inst
-}
-
-
-def calc(reg,regs)
-  val = regs[reg]
-  # p ["v", val]
-  if val == 0 || val == 1
-    return val 
-  end
-  p1, op , p2 = val.split
-  # p [p1,p2]
-  p1 = calc(p1,regs)
-  p2 = calc(p2,regs)
-  # p [op, p1,p2]
-  case (op)
-  when "AND"
-    return p1 & p2
-  when "OR"
-    return p1 | p2
-  when "XOR"
-    return (p1 ^ p2)
-  end 
 end
 
-p regs.keys.filter { |it| it.start_with?('z')}.sort.reverse.map { |it| calc(it,regs) }.join("").to_i(2)
+program.lines.each do |l|
+  inst, reg = l.strip.split(' -> ')
+  regs[reg] = inst
+end
+
+def calc(reg, regs)
+  val = regs[reg]
+  return val if [0, 1].include? val
+
+  p1, op, p2 = val.split
+  p1 = calc(p1, regs)
+  p2 = calc(p2, regs)
+  case op
+  when 'AND'
+    p1 & p2
+  when 'OR'
+    p1 | p2
+  when 'XOR'
+    (p1 ^ p2)
+  end
+end
+
+p regs.keys.filter { |it| it.start_with?('z') }.sort.reverse.map { |it| calc(it, regs) }.join('').to_i(2)
