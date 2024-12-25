e3d8effade729154d7d97080277f379b6bf32cfc
Mark Janssen <mpc.janssen@gmail.com>
2024-12-25 11:57:41 +0100

    Pacify rubocop

--- 2024/25/solution.rb
+++ 2024/25/solution.rb
@@ -1,27 +1,30 @@
-items = $stdin.read.strip.split("\n\n").map {|it| it.strip.split("\n").map(&:chars)}
+# frozen_string_literal: true
 
-$locks =[]
+# rubocop:disable Style/GlobalVars
+
+$items = $stdin.read.strip.split("\n\n").map { |it| it.strip.split("\n").map(&:chars) }
+$locks = []
 $keys = []
 
-items.each { |it|
-  heights = it.transpose.map {|col| col.count("#")-1}
+$items.each do |it|
+  heights = it.transpose.map { |col| col.count('#') - 1 }
   $locks << heights if it[0][0] == '#'
   $keys << heights if it[0][0] == '.'
-
-}
-
-def fit(key, lock)
-   key.zip(lock).each { |kh, lh |
-      return false if kh+lh > 5
-
-   }
-   return true
 end
 
-part1 = $keys.map {|k|
-  $locks.map{|l|
-    fit(k,l)
-  }
-}.flatten(1).count(true)
+def fit(key, lock)
+  key.zip(lock).each do |kh, lh|
+    return false if kh + lh > 5
+  end
+  true
+end
+
+part1 = $keys.map do |k|
+  $locks.map do |l|
+    fit(k, l)
+  end
+end.flatten(1).count(true)
 
 p part1
+
+# rubocop:enable Style/GlobalVars
