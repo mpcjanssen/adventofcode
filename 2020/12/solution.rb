3b14ca9ed97e7c4a5e9b374d8fc6abc264292a74
Mark Janssen <mpc.janssen@gmail.com>
2024-12-25 21:31:02 +0100

    2020-12: Complex rules

--- 2020/12/solution.rb
+++ 2020/12/solution.rb
@@ -8,8 +8,8 @@ def step(ship, instruction)
   pos, dir = ship
   op = instruction[0]
   arg = instruction[1..].to_i
-  p [pos, dir]
-  p [op, arg]
+  # p [pos, dir]
+  # p [op, arg]
   case op
   when 'N'
     [pos - Complex::I * arg, dir]
@@ -39,5 +39,44 @@ end
 
 $instructions.each do |inst|
   $ship = step($ship, inst)
+end
+p $ship[0].real.abs + $ship[0].imag.abs
+
+def step2(ship, instruction)
+  pos, wp = ship
+  op = instruction[0]
+  arg = instruction[1..].to_i
+  # p [pos, wp]
+  # p [op, arg]
+  case op
+  when 'N'
+    [pos, wp - Complex::I * arg]
+  when 'S'
+    [pos, wp + Complex::I * arg]
+  when 'E'
+    [pos, wp + arg]
+  when 'W'
+    [pos, wp - arg]
+  when 'F'
+    [pos + arg * wp, wp]
+  when 'R'
+    raise "invalid arg #{arg}" if arg % 90 != 0
+
+    pow = arg / 90
+    [pos, wp * Complex::I**pow]
+  when 'L'
+    raise "invalid arg #{arg}" if arg % 90 != 0
+
+    pow = arg / 90
+    [pos, wp * (- Complex::I)**pow]
+  else
+    raise "Invalid op #{op}"
+
+  end
+end
+$ship = [Complex(0, 0), Complex(10, -1)]
+
+$instructions.each do |inst|
+  $ship = step2($ship, inst)
 end
 p $ship[0].real.abs + $ship[0].imag.abs
