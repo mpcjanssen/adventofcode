a204984f7a0106264f261156122884feaa4d596c
Mark Janssen <mpc.janssen@gmail.com>
2024-12-31 14:24:01 +0100

    2015-02: Ruby

---
+++ 2015/02/solution.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+input = $stdin.readlines
+boxes = input.map { |l| l.scan(/\d+/).map(&:to_i) }
+
+part1 = boxes.map do |b|
+  x, y, z = b.sort
+  2 * (x * y + x * z + y * z) + x * y
+end.sum
+
+part2 = boxes.map do |b|
+  x, y, z = b.sort
+  2 * (x + y) + x * y * z
+end.sum
+
+p [part1, part2]
