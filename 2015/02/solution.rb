8912d347aaf82717720dbc7bab1cc632bf23b33e
Mark Janssen <mpc.janssen@gmail.com>
2024-12-31 14:25:30 +0100

    2015-02: Ruby simplify

--- 2015/02/solution.rb
+++ 2015/02/solution.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 input = $stdin.readlines
-boxes = input.map { |l| l.scan(/\d+/).map(&:to_i) }
+boxes = input.map { |l| l.scan(/\d+/).map(&:to_i).sort }
 
-part1 = boxes.map do |b|
-  x, y, z = b.sort
+part1 = boxes.map do |x, y, z|
   2 * (x * y + x * z + y * z) + x * y
 end.sum
 
-part2 = boxes.map do |b|
-  x, y, z = b.sort
+part2 = boxes.map do |x, y, z|
   2 * (x + y) + x * y * z
 end.sum
 
