d38582b08be8b9e8cdba953e0df2f4b0969d4ba2
Mark Janssen <mpc.janssen@gmail.com>
2025-01-03 02:26:02 +0100

    2024: Tcl subset

--- 2024/02/solution.tcl
+++ 2024/02/solution.tcl
@@ -11,20 +11,20 @@ proc versions report {
     return $versions
 }
 
+proc subset {s1 s2} {
+    foreach s $s1 {
+        if {$s ni $s2} {return 0}
+    }
+    return 1
+}
+
 proc issafe report {
     set deltas [lmap a [lrange $report 0 end-1] b [lrange $report 1 end] {
         expr {$b - $a}
     }]
-    if {[lindex $deltas 0] > 0} {
-        foreach d $deltas {
-            if {$d ni {1 2 3}} {return 0}
-        }
-    } else {
-        foreach d $deltas {
-            if {$d ni {-1 -2 -3}} {return 0}
-        }
-    }
-    return 1
+    if {[lindex $deltas 0] > 0 && [subset $deltas {1 2 3}]} {return 1}
+    if {[lindex $deltas 0] < 0 && [subset $deltas {-1 -2 -3}]} {return 1}
+    return 0
 }
 
 proc issafe2 {report} {
